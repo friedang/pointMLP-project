92b020f984cad59f243aab2524003170b4935237
friedang <157491259+friedang@users.noreply.github.com>
2024-02-04 22:48:31 +0100

    Add color features to pointMLP.py

--- part_segmentation/model/pointMLP.py
+++ part_segmentation/model/pointMLP.py
@@ -386,9 +386,9 @@ class PointMLP(nn.Module):
 
         self.act = get_activation(activation)
 
-        # class label mapping
-        self.cls_map = nn.Sequential(
-            ConvBNReLU1D(16, cls_dim, bias=bias, activation=activation),
+        # color feature mapping
+        self.col_map = nn.Sequential(
+            ConvBNReLU1D(3, cls_dim, bias=bias, activation=activation),
             ConvBNReLU1D(cls_dim, cls_dim, bias=bias, activation=activation)
         )
         # global max pooling mapping
@@ -400,15 +400,15 @@ class PointMLP(nn.Module):
         # classifier
         self.classifier = nn.Sequential(
             #nn.Conv1d(gmp_dim+cls_dim+de_dims[-1], 128, 1, bias=bias),
-            nn.Conv1d(128+gmp_dim, 64, 1, bias=bias),
-            nn.BatchNorm1d(64),
+            nn.Conv1d(gmp_dim+cls_dim+de_dims[-1], 128, 1, bias=bias),
+            nn.BatchNorm1d(128),
             nn.Dropout(),
-            nn.Conv1d(64, num_classes, 1, bias=bias)
+            nn.Conv1d(128, num_classes, 1, bias=bias)
         )
         self.en_dims = en_dims
 
     #def forward(self, x, norm_plt, cls_label):
-    def forward(self, x, norm_plt):
+    def forward(self, x, norm_plt, color):
         xyz = x.permute(0, 2, 1)
         x = torch.cat([x,norm_plt],dim=1)
         x = self.embedding(x)  # B,D,N
@@ -438,10 +438,11 @@ class PointMLP(nn.Module):
             gmp_list.append(F.adaptive_max_pool1d(self.gmp_map_list[i](x_list[i]), 1))
         global_context = self.gmp_map_end(torch.cat(gmp_list, dim=1)) # [b, gmp_dim, 1]
 
-        #here is the cls_token
-        #cls_token = self.cls_map(cls_label.unsqueeze(dim=-1))  # [b, cls_dim, 1]
-        #x = torch.cat([x, global_context.repeat([1, 1, x.shape[-1]]), cls_token.repeat([1, 1, x.shape[-1]])], dim=1)
-        x = torch.cat([x, global_context.repeat([1, 1, x.shape[-1]])], dim=1)
+        # here is the color context
+        color_context = F.adaptive_max_pool1d(self.col_map(color), 1)  # [b, cls_dim, 1]
+        
+        x = torch.cat([x, global_context.repeat([1, 1, x.shape[-1]]), color_context.repeat([1, 1, x.shape[-1]])], dim=1)
+        # x = torch.cat([x, global_context.repeat([1, 1, x.shape[-1]])], dim=1)
         x = self.classifier(x)
         x = F.log_softmax(x, dim=1)
         x = x.permute(0, 2, 1)
